da7404291dad19b9048786f4426497fd34f80c30
VictorVilelaDuarte <victorvduarte@gmail.com>
2020-04-06 21:11:20 -0300

    Conexão com MySQL

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import bd from '../config/database';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'The server is running!' });
+  bd.query('SELECT * FROM ens_evento', (err, result) => {
+    if (err) {
+      return res.json({ errror: 'Não foi possível buscar as informações' });
+    }
+    return res.json({ message: result });
+  });
 });
 
 export default routes;
