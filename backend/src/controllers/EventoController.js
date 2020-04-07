49e990e7ce09fcea849e6c39df5b586aa93c5dfa
VictorVilelaDuarte <victorvduarte@gmail.com>
2020-04-06 23:25:36 -0300

    CRUD evento

---
+++ backend/src/controllers/EventoController.js
@@ -0,0 +1,94 @@
+import bd from '../../config/database';
+
+class EventoController {
+  lista(req, res) {
+    bd.query('SELECT * FROM ens_evento', (err, result) => {
+      if (err) {
+        return res.status(400).json({
+          staus: false,
+          message: 'Não foi possível buscar o evento.',
+        });
+      }
+      return res.status(200).json({
+        status: true,
+        data: result,
+      });
+    });
+  }
+
+  insere(req, res) {
+    const { data, hora, local, equipe, descricao, historico, tipo } = req.body;
+    bd.query(
+      `INSERT INTO ens_evento (Evento_Data, Evento_Horario, Evento_Local, Evento_TipoID, Evento_EquipeResp, Evento_Descricao, Evento_Historico) values ('${data}', '${hora}', '${local}', '${tipo}', '${equipe}', '${descricao}', '${historico}')`,
+      (err) => {
+        if (err) {
+          return res.status(400).json({
+            staus: false,
+            message: 'Não foi possível salvar o evento.',
+          });
+        }
+        return res.status(200).json({
+          status: true,
+          message: 'Evento registrado com sucesso!',
+        });
+      }
+    );
+  }
+
+  altera(req, res) {
+    const { id } = req.params;
+    const { data, hora, local, equipe, descricao, historico, tipo } = req.body;
+    bd.query(
+      `UPDATE ens_evento SET Evento_Data='${data}', Evento_Horario='${hora}', Evento_Local='${local}', Evento_TipoID='${tipo}', Evento_EquipeResp='${equipe}', Evento_Descricao='${descricao}', Evento_Historico='${historico}' WHERE Evento_ID='${id}'`,
+      (err) => {
+        if (err) {
+          return res.status(400).json({
+            staus: false,
+            message: 'Não foi possível salvar o evento.',
+          });
+        }
+        return res.status(200).json({
+          status: true,
+          message: 'Evento atualizado com sucesso!',
+        });
+      }
+    );
+  }
+
+  busca(req, res) {
+    const { id } = req.params;
+    bd.query(
+      `SELECT * FROM ens_evento WHERE Evento_ID=${id}`,
+      (err, result) => {
+        if (err) {
+          return res.status(400).json({
+            staus: false,
+            message: 'Não foi possível buscar o evento.',
+          });
+        }
+        return res.status(200).json({
+          status: true,
+          data: result,
+        });
+      }
+    );
+  }
+
+  deleta(req, res) {
+    const { id } = req.params;
+    bd.query(`DELETE FROM ens_evento WHERE Evento_ID=${id}`, (err, result) => {
+      if (err) {
+        return res.status(400).json({
+          staus: false,
+          message: 'Não foi possível excluir o evento.',
+        });
+      }
+      return res.status(200).json({
+        status: true,
+        message: 'Evento deletado com sucesso!',
+      });
+    });
+  }
+}
+
+export default new EventoController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
-import bd from '../config/database';
+import EventoController from './controllers/EventoController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  bd.query('SELECT * FROM ens_evento', (err, result) => {
-    if (err) {
-      return res.json({ errror: 'Não foi possível buscar as informações' });
-    }
-    return res.json({ message: result });
-  });
+  return res.json({ server_status: 'ok' });
 });
+
+routes.get('/evento', EventoController.lista);
+routes.get('/evento/:id', EventoController.busca);
+routes.post('/evento', EventoController.insere);
+routes.put('/evento/:id', EventoController.altera);
+routes.delete('/evento/:id', EventoController.deleta);
 
 export default routes;
