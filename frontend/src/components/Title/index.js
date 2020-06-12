a8d7f90c9f33819f10e73d347ee48169e83fd6eb
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-06-12 17:55:12 -0300

    Ajuste botão voltar dos titulos

--- frontend/src/components/Title/index.js
+++ frontend/src/components/Title/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 
-import { Container, TitleP } from './styles';
+import { Container, TitleP, BackButton } from './styles';
 
 function Title({ back, children }) {
   return (
     <Container>
       {back && (
         <Link to={back}>
-          <FaArrowLeft color="#f4f4f4" />
+          <BackButton>
+            <FaArrowLeft color="#717171" size={20} />
+          </BackButton>
         </Link>
       )}
       <TitleP>{children}</TitleP>
--- frontend/src/components/Title/styles.js
+++ frontend/src/components/Title/styles.js
@@ -3,10 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   justify-content: flex-start;
-  align-items: center;
+  /* align-items: center; */
   width: 50%;
 `;
 export const TitleP = styled.p`
   color: #0377d1;
   font-size: 26px;
+`;
+
+export const BackButton = styled.button`
+  height: 24px;
+  width: 30px;
+  border: none;
 `;
