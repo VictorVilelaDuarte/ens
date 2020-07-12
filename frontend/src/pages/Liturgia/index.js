a95440b99a38db003148607ace30d79e9413380c
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-07-11 21:46:35 -0300

    Finalização página liturgia

--- frontend/src/pages/Liturgia/index.js
+++ frontend/src/pages/Liturgia/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, SubTitle, Item } from './styles';
 
 import Title from '../../components/Title';
 
 function Liturgia() {
-  return <Title> Liturgia </Title>;
+  return (
+    <Container>
+      <Title> Liturgia </Title>
+      <SubTitle>1. Liturgia Diária:</SubTitle>
+      <Item>
+        (Fonte:
+        <a href="https://enscacapava.com.br/liturgia.php"> Católico Orante </a>)
+      </Item>
+      <Item>
+        (Fonte:
+        <a href="http://liturgiadiaria.cnbb.org.br/app/user/user/UserView.php">
+          {' '}
+          CNBB{' '}
+        </a>
+        )
+      </Item>
+      <SubTitle>2. Notícias do Vaticano:</SubTitle>
+      <Item>
+        (Fonte:
+        <a href="https://pt.zenit.org/"> Zenit.org</a>)
+      </Item>
+      <SubTitle>3. Nossa Diocese</SubTitle>
+      <Item>
+        (Fonte:
+        <a href="http://diocesedetaubate.org.br/categoria/paroquias/cacapava/">
+          {' '}
+          Diocese de Taubaté - Caçapava
+        </a>
+        )
+      </Item>
+    </Container>
+  );
 }
 
 export default Liturgia;
--- frontend/src/pages/Liturgia/styles.js
+++ frontend/src/pages/Liturgia/styles.js
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  width: 100%;
+`;
+
+export const SubTitle = styled.p`
+  color: #70cae3;
+  font-size: 22px;
+  font-weight: bold;
+  margin-top: 20px;
+`;
+
+export const Item = styled.p`
+  font-size: 17px;
+  margin-left: 25px;
+`;
