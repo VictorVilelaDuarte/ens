2c46855c3eef2ab0517d768436ce0718fde1cbe1
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-07-19 19:46:44 -0300

    Novo endpoint cadastro pilotagem

--- backend/src/controllers/PilotagemController.js
+++ backend/src/controllers/PilotagemController.js
@@ -82,15 +82,15 @@ class PilotagemController {
         Pilot_IgrejaCasamento, Pilot_Endereco, Pilot_EndBairro, Pilot_EndCEP, Pilot_Cidade, Pilot_Estado, Pilot_Pais,
         Pilot_TelResid, Pilot_DataInicioENS, Pilot_NomeFilho1, Pilot_DataNascFilho1, Pilot_NomeFilho2, Pilot_DataNascFilho2,
         Pilot_NomeFilho3, Pilot_DataNascFilho3, Pilot_NomeFilho4, Pilot_DataNascFilho4, Pilot_NomeFilho5, Pilot_DataNascFilho5,
-        Pilot_ExpComID, Pilot_AnoExpCom)
-        VALUES (${Pilot_IDMENS}, ${Pilot_HomemID}, ${Pilot_HomemNome}, ${Pilot_HomemEmail}, ${Pilot_HomemTelCel},
-        ${Pilot_HomemTelCom}, ${Pilot_HomemDataNascimento}, ${Pilot_MulherID}, ${Pilot_MulherNome}, ${Pilot_MulherEmail},
-        ${Pilot_MulherTelCel}, ${Pilot_MulherTelCom}, ${Pilot_MulherDataNascimento}, ${Pilot_Nome}, ${Pilot_DataCasamento},
-        ${Pilot_ParoquiaCasamento}, ${Pilot_IgrejaCasamento}, ${Pilot_Endereco}, ${Pilot_EndBairro}, ${Pilot_EndCEP},
-        ${Pilot_Cidade}, ${Pilot_Estado}, ${Pilot_Pais}, ${Pilot_TelResid}, ${Pilot_DataInicioENS},
-        ${Pilot_NomeFilho1}, ${Pilot_DataNascFilho1}, ${Pilot_NomeFilho2}, ${Pilot_DataNascFilho2}, ${Pilot_NomeFilho3},
-        ${Pilot_DataNascFilho3}, ${Pilot_NomeFilho4}, ${Pilot_DataNascFilho4}, ${Pilot_NomeFilho5}, ${Pilot_DataNascFilho5},
-        ${Pilot_ExpComID}, ${Pilot_AnoExpCom});`,
+        Pilot_ExpComID, Pilot_AnoExpCom, Pilot_EquipeID)
+        VALUES ('${Pilot_IDMENS}', '${Pilot_HomemID}', '${Pilot_HomemNome}', '${Pilot_HomemEmail}', '${Pilot_HomemTelCel}',
+        '${Pilot_HomemTelCom}', '${Pilot_HomemDataNascimento}', '${Pilot_MulherID}', '${Pilot_MulherNome}', '${Pilot_MulherEmail}',
+        '${Pilot_MulherTelCel}', '${Pilot_MulherTelCom}', '${Pilot_MulherDataNascimento}', '${Pilot_Nome}', '${Pilot_DataCasamento}',
+        '${Pilot_ParoquiaCasamento}', '${Pilot_IgrejaCasamento}', '${Pilot_Endereco}', '${Pilot_EndBairro}', '${Pilot_EndCEP}',
+        '${Pilot_Cidade}', '${Pilot_Estado}', '${Pilot_Pais}', '${Pilot_TelResid}', '${Pilot_DataInicioENS}',
+        '${Pilot_NomeFilho1}', '${Pilot_DataNascFilho1}', '${Pilot_NomeFilho2}', '${Pilot_DataNascFilho2}', '${Pilot_NomeFilho3}',
+        '${Pilot_DataNascFilho3}', '${Pilot_NomeFilho4}', '${Pilot_DataNascFilho4}', '${Pilot_NomeFilho5}', '${Pilot_DataNascFilho5}',
+        '${Pilot_ExpComID}', '${Pilot_AnoExpCom}', '0');`,
       (err) => {
         if (err) {
           return res.status(400).json({
