07eb643ad6e9c77630c217ef8f8a68aa9d6f5312
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-07-28 21:16:12 -0300

    Endpoint listagem de informens

--- backend/src/controllers/InformensController.js
+++ backend/src/controllers/InformensController.js
@@ -2,62 +2,21 @@ import bd from '../../config/database';
 
 class InformensController {
   async lista(req, res) {
-    let { page } = req.query;
-    let offset = 0;
-    let numberOfPages = 0;
-    page = parseInt(page);
-    if (!page) {
-      page = 1;
-    }
-
-    bd.query('SELECT COUNT (*) as total from ens_informens', (err, result) => {
-      if (err) {
-        return res.status(400).json({
-          status: false,
-          message: 'Não foi possivel buscar os informens.',
+    bd.query(
+      `SELECT * FROM ens_informens ORDER BY informens_data`,
+      (error, results) => {
+        if (error) {
+          return res.status(400).json({
+            staus: false,
+            message: 'Não foi possível buscar os informens.',
+          });
+        }
+        return res.status(200).json({
+          status: true,
+          data: results,
         });
       }
-      const reminder = result[0].total % 10;
-      if (reminder) {
-        numberOfPages = parseInt(result[0].total / 10) + 1;
-      } else {
-        numberOfPages = result[0].total / 10;
-      }
-      if (page < 1) {
-        page = 1;
-      }
-      if (page > numberOfPages) {
-        page = numberOfPages;
-      }
-
-      if (page > 1) {
-        offset = (page - 1) * 10;
-      }
-
-      bd.query(
-        `SELECT * FROM ens_informens ORDER BY informens_data DESC LIMIT 10 OFFSET ${offset} `,
-        (error, results) => {
-          if (error) {
-            return res.status(400).json({
-              staus: false,
-              message: 'Não foi possível buscar os informens.',
-            });
-          }
-          return res
-            .status(200)
-            .header({
-              prevPage: page <= 1 ? page : page - 1,
-              page,
-              nextPage: numberOfPages > page ? page + 1 : page,
-              lastPage: numberOfPages,
-            })
-            .json({
-              status: true,
-              data: results,
-            });
-        }
-      );
-    });
+    );
   }
 
   insere(req, res) {
