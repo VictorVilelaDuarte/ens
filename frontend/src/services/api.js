9d4e370a92c3b3329c7458de24f8efcaef14a16f
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-08-15 04:12:35 -0300

    Arrumando endpoint de produção

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.16:3333',
+  baseURL: 'http://167.172.157.226:3333',
 });
 
 api.interceptors.request.use(async (config) => {
