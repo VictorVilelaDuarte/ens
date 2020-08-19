f2adfd52381188d7e89ddb0d19c761b676a4a546
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-08-19 01:39:31 -0300

    Ajustando arquivos de imagem

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,46 +18,96 @@ class App {
 
     this.server.use(express.json());
     this.server.use(cors(corsOptions));
-    this.server.use(
-      '/files-capa',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'capa'))
-    );
-    this.server.use(
-      '/files-galeria',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'galeria'))
-    );
-    this.server.use(
-      '/files-informens',
-      express.static(
-        path.resolve(__dirname, '..', 'tmp', 'uploads', 'informens')
-      )
-    );
-    this.server.use(
-      '/files-oracao',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'oracao'))
-    );
-    this.server.use(
-      '/files-noticia',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'noticia'))
-    );
-    this.server.use(
-      '/files-equipe',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'equipe'))
-    );
-    this.server.use(
-      '/files-conselheiro',
-      express.static(
-        path.resolve(__dirname, '..', 'tmp', 'uploads', 'conselheiro')
-      )
-    );
-    this.server.use(
-      '/files-arquivo',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'arquivo'))
-    );
-    this.server.use(
-      '/files-casal',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'casal'))
-    );
+
+    if (process.env.APP_URL === 'http://localhost:3333') {
+      this.server.use(
+        '/files-capa',
+        express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'capa'))
+      );
+      this.server.use(
+        '/files-galeria',
+        express.static(
+          path.resolve(__dirname, '..', 'tmp', 'uploads', 'galeria')
+        )
+      );
+      this.server.use(
+        '/files-informens',
+        express.static(
+          path.resolve(__dirname, '..', 'tmp', 'uploads', 'informens')
+        )
+      );
+      this.server.use(
+        '/files-oracao',
+        express.static(
+          path.resolve(__dirname, '..', 'tmp', 'uploads', 'oracao')
+        )
+      );
+      this.server.use(
+        '/files-noticia',
+        express.static(
+          path.resolve(__dirname, '..', 'tmp', 'uploads', 'noticia')
+        )
+      );
+      this.server.use(
+        '/files-equipe',
+        express.static(
+          path.resolve(__dirname, '..', 'tmp', 'uploads', 'equipe')
+        )
+      );
+      this.server.use(
+        '/files-conselheiro',
+        express.static(
+          path.resolve(__dirname, '..', 'tmp', 'uploads', 'conselheiro')
+        )
+      );
+      this.server.use(
+        '/files-arquivo',
+        express.static(
+          path.resolve(__dirname, '..', 'tmp', 'uploads', 'arquivo')
+        )
+      );
+      this.server.use(
+        '/files-casal',
+        express.static(path.resolve(__dirname, '..', 'tmp', 'uploads', 'casal'))
+      );
+    } else {
+      this.server.use(
+        '/files-capa',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'capa'))
+      );
+      this.server.use(
+        '/files-galeria',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'galeria'))
+      );
+      this.server.use(
+        '/files-informens',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'informens'))
+      );
+      this.server.use(
+        '/files-oracao',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'oracao'))
+      );
+      this.server.use(
+        '/files-noticia',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'noticia'))
+      );
+      this.server.use(
+        '/files-equipe',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'equipe'))
+      );
+      this.server.use(
+        '/files-conselheiro',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'conselheiro'))
+      );
+      this.server.use(
+        '/files-arquivo',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'arquivo'))
+      );
+      this.server.use(
+        '/files-casal',
+        express.static(path.resolve(__dirname, 'tmp', 'uploads', 'casal'))
+      );
+    }
   }
 
   routes() {
