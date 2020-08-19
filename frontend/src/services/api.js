728c0f7fdecc6d87e59566af279188abc945aada
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-08-19 00:31:32 -0300

    Voltando o service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.16:3333',
+  baseURL: 'https://apiens.servile.com.br',
 });
 
 api.interceptors.request.use(async (config) => {
