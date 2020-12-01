20a5b10d25ac41f5b47a6624d652e74f831a964b
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-12-01 00:06:41 -0300

    Busca do erro de informens

--- backend/src/config/multerInformens.js
+++ backend/src/config/multerInformens.js
@@ -7,7 +7,10 @@ export default {
     destination: resolve(__dirname, '..', 'tmp', 'uploads', 'informens'),
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
-        if (err) return cb(err);
+        if (err) {
+          console.log(err);
+          return cb(err);
+        }
 
         return cb(null, res.toString('hex') + extname(file.originalname));
       });
